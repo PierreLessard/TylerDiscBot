380d192c2bab819274e689bcdb755b9f6c9b218d
Pierre <Lessardpw@gmail.com>
2020-09-07 01:01:35 -0400

    better change function

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from key import give_bot_key
+import re
 
 key = give_bot_key()
 client = discord.Client()
@@ -13,22 +14,24 @@ async def on_ready():
 async def on_message(message):
     mb = mbl[0] #the beginning of a message
 
-    
     if message.author == client.user:
         return
 
-    if message.content[0] != mb:
+    if message.content.startswith(mb) == False:
         return
 
-    print(message.content)
-    print(message.content[0])
 
     if message.content.startswith(mb+'help'):
         await message.channel.send('Commands:')
-        await message.channel.send("{}change \"new_prefix\" ----- This will change how I register commands".format(mb))
+        await message.channel.send("{}change new_prefix ----- This will change how I register commands".format(mb))
     
-    if message.content.startswith(mb+'change'): 
-        mbl[0] = message.content[len(message.content)-1]
-        await message.channel.send("The command prefix has been changed to: " +  mbl[0])
+    if message.content.startswith(mb+'change'):
+        txt = message.content
+        command = re.compile(r'.*change\s*')
+        mbl[0] = command.sub('',txt)
+        await message.channel.send("The command prefix has been changed to: {}".format(mbl[0]))
+
+    if message.content.startswith(mb+'reassure me'):
+        await message.channel.send("Ok {}, It's all going good. Trust me, I am a robot".format(message.author.display_name))
 
 client.run(key)
